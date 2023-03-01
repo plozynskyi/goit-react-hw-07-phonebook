be6e74c41568bb2db881676a0df3f7b8cc3f7160
Павел <p.lozynskyi@gmail.com>
2023-03-02 01:41:11 +0200

    fin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import App from 'components/App/App';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+
+import './shared/styles/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
